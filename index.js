d688b8dff3139712821fea45816907edb4b604e5
hs <67200805+hs-x11@users.noreply.github.com>
2023-11-11 18:31:11 -0500

    Added small changes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import prompt from "readline-sync";
 import wordBank from "./word-bank.js";
 
-
+const words = wordBank;
+const alphabet = "ABCDEFGHIJKLMOPQRSTUVWXYZ".split('');
 
 console.log("\nWelcome to Hangman!\nPress ctrl+c to stop\n");
 
-const letter = prompt.question("Please guess a letter: ");
+const guess = prompt.question("Please guess a letter: ");
